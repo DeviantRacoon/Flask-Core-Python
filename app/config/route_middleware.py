1b8592d8256e2aeb733f454a379d74517be9aaf0
JHectorZ <101916534+JHectorZ@users.noreply.github.com>
2023-10-18 13:09:26 -0700

    [ CORS ] Se ha implementado correctamente los CORS (#3)

--- app/config/route_middleware.py
+++ app/config/route_middleware.py
@@ -1,11 +1,12 @@
 from flask import Flask
-from flask_sqlalchemy import SQLAlchemy
+from flask_cors import CORS
 from .database import setConfig
 from .dotenv import env
 from modules.core.infrastructure.routes import person_route, user_route
 from .jwt import setConfigJWT
 
 app = Flask(env['NAME_APP'], template_folder = 'app/modules/core/infrastructure/templates')
+CORS(app, resources={r"*": {"origins": env['CROSS_ORIGIN']}})
 
 setConfig(app, env)
 jwt = setConfigJWT(app, env)
--- app/index.py
+++ app/index.py
@@ -2,9 +2,11 @@ from config.dotenv import env
 from utils.errors_handlers import app
 from config.database import database as db
 from config.logging import logger
+from flask_cors import cross_origin
 
 
 @app.route('/')
+@cross_origin()
 def index():
     # return make_response(render_template('index.html', title = env['NAME_APP']), 200)
     return {"ok": True, "message": "Se pudo"}
